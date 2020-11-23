3b5649a30326532242c285eb2e14ca7f54d89fbf
monia-souid <monia.souid@gmail.com>
2020-11-23 11:48:04 +0100

    first commit

---
+++ src/Description.js
@@ -0,0 +1,31 @@
+
+import React, { useEffect, useState } from 'react';
+
+
+function Description({ movies, match }) {
+    const [movie, setMovie] = useState(null);
+    useEffect(() => {
+        setMovie(
+            movies.filter((el) => el.title === match.params.name)[0]
+        );
+    });
+    return (
+        <div className="col-4">
+            <div style={{textAlign :`center`,color:`white`, height:`300px`}} >
+                {
+                    movie && <div style= {{backgroundImage : ` linear-gradient(90deg, rgba(185,185,185,0.1) 0%, rgba(147,147,147,0.1) 100%) , url(\"${movie.image}\")`}} className="description">
+
+                        <h2>{movie.title}</h2>
+                        <p>{movie.description}</p>
+                        <p>{movie.rate}</p>
+                        <p>{movie.url}</p>
+                    </div>
+                }</div>
+
+
+        </div>
+    );
+}
+
+export default Description
+
